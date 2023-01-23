890b893a0e26669cf2b35b8789dece05aec04fc8
Moustafa Hammoud <moustafa.hammoud01@lau.edu>
2023-01-23 20:00:37 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -2,6 +2,7 @@ public class Me{
 	public static void main(String[] args){
 		System.out.println("My name is: Moustafa Hammoud");
 		System.out.println("My major is Electrical Engineering");
+		System.out.println("I am a first year student");
 
 }
 }
