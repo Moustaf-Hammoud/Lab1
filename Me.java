a9d208c8df83b4b3532374f11c6458a246d09726
Moustafa Hammoud <moustafa.hammoud01@lau.edu>
2023-01-23 19:56:35 +0200

    My name and major

---
+++ Me.java
@@ -0,0 +1,7 @@
+public class Me{
+	public static void main(String[] args){
+		System.out.println("My name is: Moustafa Hammoud");
+		System.out.println("My major is Electrical Engineering");
+
+}
+}
